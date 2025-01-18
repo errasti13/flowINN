2729d8b2561698a5507ef0a0152dfef1c4d34475
Jon Errasti <jon44errasti@gmail.com>
2025-01-18 10:44:29 +0100

    Created new plot class and moved all plotting functions.

--- examples/LidDrivenCavity.py
+++ examples/LidDrivenCavity.py
@@ -1,17 +1,22 @@
 from src.tests.LidDrivenCavity import LidDrivenCavity
 
+
 def main():
     x_range = (-1, 1)
     y_range = (-1, 1)
 
     equation = LidDrivenCavity('LidDrivenCavity', x_range, y_range)
     
-    equation.generateMesh(Nx = 300, Ny = 300)
+    equation.generateMesh(Nx = 60, Ny = 100)
 
-    equation.train(epochs=100000)
+    #equation.train(epochs=1000)
+
+    equation.model.load("trainedModels/" + equation.problemTag + ".tf")
 
     equation.predict()
-    equation.plot()
+    equation.plot('u')
+    equation.plot('v')
+    equation.plot('p')
 
     return
 
---
+++ src/plot/plot.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Plot:
+
+    def __init__(self, mesh):
+        self.X = mesh.X
+        self.Y = mesh.Y
+
+        if mesh.is2D == False:
+            self.Z = mesh.Z
+
+        self.solutions = mesh.solutions
+
+    def plot(self, solkey):
+        from scipy.interpolate import griddata
+        # Assuming self.mesh.solutions['sol'] is of size (5000,)
+        x = self.X
+        y = self.Y
+        sol = self.solutions[solkey]
+
+        grid_x, grid_y = np.meshgrid(np.linspace(x.min(), x.max(), 100), np.linspace(y.min(), y.max(), 100))
+
+        # Interpolate the scattered data to the grid using griddata
+        grid_sol = griddata((x, y), sol, (grid_x, grid_y), method='cubic')
+
+        # Plot the result using contourf
+        plt.figure(figsize=(8, 6))
+        plt.title(f'Solution Field {solkey}')
+        cp = plt.contourf(grid_x, grid_y, grid_sol, cmap='jet', levels=50)
+        plt.colorbar(cp)
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.axis('equal')
+        plt.tight_layout()
+        plt.show()
+
