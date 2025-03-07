3a15994f82b73f46bb94d544677998fa90460a2a
Jon Errasti <jon44errasti@gmail.com>
2025-03-07 19:48:53 +0100

    refactor(physics): split Navier-Stokes implementations into separate files
    - Move 2D and 3D Navier-Stokes implementations to dedicated files
    - Rename classes to SteadyNavierStokes2D and SteadyNavierStokes3D
    - Keep base NavierStokes abstract class in navier_stokes.py
    - Update imports to use relative imports from base class

---
+++ flowinn/physics/steady_2D.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from typing import Tuple
+from .steadyNS import NavierStokes
+
+class UnsteadyNavierStokes2D(NavierStokes):
+    """2D Unsteady Navier-Stokes equations solver."""
+
+    def get_residuals(self, velocities: tf.Tensor, pressure: tf.Tensor, coords: list, tape) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+        """
+        Calculate 2D Unsteady Navier-Stokes residuals.
+        
+        Args:
+            velocities: Tensor of velocity components [u, v]
+            pressure: Pressure tensor
+            coords: List of coordinate tensors [x, y]
+            tape: Gradient tape for automatic differentiation
+            
+        Returns:
+            Tuple[tf.Tensor, tf.Tensor, tf.Tensor]: (continuity, momentum_x, momentum_y) residuals
+        """
+        x, y = coords
+        u = velocities[:, 0]
+        v = velocities[:, 1]
+        
+        tape.watch([x, y])
+        
+        # First derivatives
+        [u_x, u_y, v_x, v_y, p_x, p_y] = self._compute_first_derivatives(
+            [u, v, pressure], [x, y], tape
+        )
+        
+        # Second derivatives
+        [u_xx, u_xy, u_yx, u_yy, v_xx, v_xy, v_yx, v_yy] = self._compute_second_derivatives(
+            [u_x, u_y, v_x, v_y], [x, y], tape
+        )
+
+        continuity = u_x + v_y
+        
+        momentum_x = (
+            u * u_x + v * u_y +
+            p_x -
+            self.nu * (u_xx + u_yy)
+        )
+        
+        momentum_y = (
+            u * v_x + v * v_y +
+            p_y -
+            self.nu * (v_xx + v_yy)
+        )
+
+        continuity = tf.reshape(continuity, [-1])
+        momentum_x = tf.reshape(momentum_x, [-1])
+        momentum_y = tf.reshape(momentum_y, [-1])
+
+        return continuity, momentum_x, momentum_y
---
+++ flowinn/physics/steady_3D.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from typing import Tuple
+from .steadyNS import NavierStokes
+
+class UnsteadyNavierStokes3D(NavierStokes):
+    """3D Unsteady Navier-Stokes equations solver."""
+
+    def get_residuals(self, velocities: tf.Tensor, pressure: tf.Tensor, coords: list, tape) -> Tuple[tf.Tensor, ...]:
+        """
+        Calculate 3D Unsteady Navier-Stokes residuals.
+        
+        Args:
+            velocities: Tensor of velocity components [u, v, w]
+            pressure: Pressure tensor
+            coords: List of coordinate tensors [x, y, z]
+            tape: Gradient tape for automatic differentiation
+            
+        Returns:
+            Tuple[tf.Tensor, ...]: (continuity, momentum_x, momentum_y, momentum_z) residuals
+        """
+        x, y, z = coords
+        u = velocities[:, 0]
+        v = velocities[:, 1]
+        w = velocities[:, 2]
+        
+        tape.watch([x, y, z])
+        
+        [u_x, u_y, u_z, v_x, v_y, v_z, w_x, w_y, w_z, p_x, p_y, p_z] = self._compute_first_derivatives(
+            [u, v, w, pressure], [x, y, z], tape
+        )
+        
+        [u_xx, u_yy, u_zz, v_xx, v_yy, v_zz, w_xx, w_yy, w_zz] = [
+            tape.gradient(d, c) for d, c in [
+                (u_x, x), (u_y, y), (u_z, z),
+                (v_x, x), (v_y, y), (v_z, z),
+                (w_x, x), (w_y, y), (w_z, z)
+            ]
+        ]
+
+        continuity = u_x + v_y + w_z
+
+        momentum_x = u * u_x + v * u_y + w * u_z + p_x - self.nu * (u_xx + u_yy + u_zz)
+        momentum_y = u * v_x + v * v_y + w * v_z + p_y - self.nu * (v_xx + v_yy + v_zz)
+        momentum_z = u * w_x + v * w_y + w * w_z + p_z - self.nu * (w_xx + w_yy + w_zz)
+
+        continuity = tf.reshape(continuity, [-1])
+        momentum_x = tf.reshape(momentum_x, [-1])
+        momentum_y = tf.reshape(momentum_y, [-1])
+        momentum_z = tf.reshape(momentum_z, [-1])
+
+        return continuity, momentum_x, momentum_y, momentum_z
