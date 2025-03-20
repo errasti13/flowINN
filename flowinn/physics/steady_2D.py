30441cc4f39f2e3d508f93af88a4fd12019a9a93
Jon Errasti <jon44errasti@gmail.com>
2025-03-20 22:41:27 +0100

    Fixed docstring and added function decorator

--- flowinn/physics/steady_2D.py
+++ flowinn/physics/steady_2D.py
@@ -3,7 +3,7 @@ from typing import Tuple
 from .navier_stokes import NavierStokes
 
 class SteadyNavierStokes2D(NavierStokes):
-    """2D Unsteady Navier-Stokes equations solver."""
+    """2D Steady Navier-Stokes equations solver."""
 
     def __init__(self, Re: float = 1000.0):
         """
@@ -14,6 +14,7 @@ class SteadyNavierStokes2D(NavierStokes):
         """
         self.Re = Re
 
+    @tf.function
     def get_residuals(self, velocities: tf.Tensor, pressure: tf.Tensor, coords: list, tape) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
         """
         Calculate 2D Steady Navier-Stokes residuals.
@@ -32,41 +33,43 @@ class SteadyNavierStokes2D(NavierStokes):
         v = velocities[:, 1]
         p = tf.reshape(pressure, [-1])
         
-        # Watch coordinates for automatic differentiation
         tape.watch([x, y])
         
-        # First derivatives
         [u_x, u_y, v_x, v_y, p_x, p_y] = self._compute_first_derivatives(
             [u, v, p], [x, y], tape
         )
         
-        # Second derivatives for viscous terms
         [u_xx, u_xy, u_yx, u_yy, v_xx, v_xy, v_yx, v_yy] = self._compute_second_derivatives(
             [u_x, u_y, v_x, v_y], [x, y], tape
         )
 
-
         # Continuity equation: ∂u/∂x + ∂v/∂y = 0
         continuity = u_x + v_y
         
-        # Momentum equation x: u∂u/∂x + v∂u/∂y = -1/ρ * ∂p/∂x + ν(∂²u/∂x² + ∂²u/∂y²)
-        # Where ν = μ/ρ is kinematic viscosity
+        # Momentum equation x: u∂u/∂x + v∂u/∂y = -∂p/∂x + 1/Re(∂²u/∂x² + ∂²u/∂y²)
         momentum_x = (
             u * u_x + v * u_y +
             p_x -
-            1 / self.Re * (u_xx + u_yy)  # Corrected ν to 1/Re
+            1 / self.Re * (u_xx + u_yy)
         )
         
-        # Momentum equation y: u∂v/∂x + v∂v/∂y = -1/ρ * ∂p/∂y + ν(∂²v/∂x² + ∂²v/∂y²)
+        # Momentum equation y: u∂v/∂x + v∂v/∂y = -∂p/∂y + 1/Re(∂²v/∂x² + ∂²v/∂y²)
         momentum_y = (
             u * v_x + v * v_y +
             p_y  -
-            1 / self.Re * (v_xx + v_yy)  # Corrected ν to 1/Re
+            1 / self.Re * (v_xx + v_yy)
         )
-
 
         continuity = tf.reshape(continuity, [-1])
         momentum_x = tf.reshape(momentum_x, [-1])
         momentum_y = tf.reshape(momentum_y, [-1])
 
         return continuity, momentum_x, momentum_y
+
+    @tf.function
+    def _compute_first_derivatives(self, variables, coords, tape):
+        return super()._compute_first_derivatives(variables, coords, tape)
+
+    @tf.function  
+    def _compute_second_derivatives(self, first_derivs, coords, tape):
+        return super()._compute_second_derivatives(first_derivs, coords, tape)
